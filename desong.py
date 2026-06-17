4bbb20f0be610ee474b68c722779e6434a2a8d90
chlewey <chlewey@gmail.com>
2026-06-17 10:01:12 -0500

    Split

---
+++ desong.py
@@ -0,0 +1,45 @@
+import re
+
+def maketag(title):
+    tag = re.sub(r'[-\'’:\\\(\)¿\?,¡!/]', '', title.lower())
+    tag = re.sub('[ñ&]', 'n', tag)
+    tag = re.sub('á', 'a', tag)
+    tag = re.sub('é', 'e', tag)
+    tag = re.sub('í', 'i', tag)
+    tag = re.sub('[óö]', 'o', tag)
+    tag = re.sub('[úü]', 'u', tag)
+    tag = re.sub(' ', '_', tag)
+    tag = re.sub('_+', '_', tag)
+    return tag
+    
+if __name__ == '__main__':
+    import sys, os
+
+    ofilename = sys.argv[2] if len(sys.argv)>2 else None
+    ifilename = sys.argv[1] if len(sys.argv)>1 else None
+    if ifilename:
+        with open(ifilename, 'r', encoding='utf-8') as f:
+            text = f.read()
+        path, ext = os.path.splitext(ifilename)
+    else:
+        text = sys.stdin.read()
+        path, ext = '.', '.tex'
+    os.makedirs(path, exist_ok=True)
+
+    ofile = open(ofilename, 'w', encoding='utf-8') if ofilename else sys.stdout
+    
+    parts = re.split(r'\\songtitle\{([^{}]*)\}\s*', text)
+    ofile.write(parts[0])
+    for i in range(1, len(parts), 2):
+        base = maketag(parts[i])
+        fnb, fn = f'{path}/{base}', f'{path}/{base}{ext}'
+        j = 0
+        while os.path.exists(fn):
+            j += 1
+            fnb, fn = f'{path}/{base}_{j}', f'{path}/{base}_{j}{ext}'
+
+        ofile.write(f'\\import{{{fnb}}}\n')
+        with open(fn, 'w', encoding='utf-8') as f:
+            f.write(f'\\songtitle{{{parts[i]}}}\n\n')
+            f.write(parts[i+1])
+    ofile.close()
